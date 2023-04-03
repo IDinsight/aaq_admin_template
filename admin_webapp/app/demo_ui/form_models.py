8f643c4a00e088b97b4aced369154676620a8f64
Suzin You <suzinyou.sy@gmail.com>
2023-04-03 11:13:21 +0530

    update demo ui page

--- admin_webapp/app/demo_ui/form_models.py
+++ admin_webapp/app/demo_ui/form_models.py
@@ -13,6 +13,17 @@ class APICallDemoForm(FlaskForm):
         widget=TextArea(),
     )
 
+    context_1 = StringField()
+    context_2 = StringField()
+    context_3 = StringField()
+    context_4 = StringField()
+    context_5 = StringField()
+    context_6 = StringField()
+    context_7 = StringField()
+    context_8 = StringField()
+    context_9 = StringField()
+    context_10 = StringField()
+
     submit = SubmitField("Submit")
 
 
